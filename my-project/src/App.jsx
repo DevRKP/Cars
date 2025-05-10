18810ab217296be3f86b16dadbae7002779a1a11
DevRKP <145203191+DevRKP@users.noreply.github.com>
2025-05-11 01:51:00 +0530

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='h-screen bg-zinc-800'>
+      <h1 className='font-bold text-white'>Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
